4551bb700f0a6b85cdf0c28f5535da66dadbc2c1
Oona Österberg <oonaos@ety-a2123b-06.edu.wks.metropolia.fi>
2018-01-18 16:33:04 +0200

    b kohta

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,6 +9,12 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
     tulos = 'moro';
+    apitulos = 'moro taas';
+    bptulos: any;
+
+    apiosoite = 'http://media.mw.metropolia.fi/wbma';
+    kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
+    bposoite = 'http://impact.brighterplanet.com';
 
     constructor(private http: HttpClient) {
     }
@@ -24,8 +30,24 @@ export class HttpTestComponent implements OnInit {
         });
     }
 
+    getFromApi() {
+        this.http.get(this.apiosoite + '/media').subscribe(data => {
+            console.log(data[0].filename);
+            this.apitulos = this.kuvaosoite + data[0].filename;
+        });
+    }
+
+    getBP() {
+        this.http.get(this.bposoite + '/automobiles.json').subscribe(data => {
+            console.log(data);
+            this.bptulos = data;
+        });
+    }
+
     ngOnInit() {
         this.getJson();
+        this.getFromApi();
+        this.getBP();
     }
 
 }
